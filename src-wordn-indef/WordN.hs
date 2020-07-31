7e0973d75fe26a143b684d957f4ca65cf74c3982
Zachary Churchill <zacharyachurchill@gmail.com>
2020-07-30 20:49:35 -0400

    NoMask newtype

--- src-wordn-indef/WordN.hs
+++ src-wordn-indef/WordN.hs
@@ -12,8 +12,11 @@
 
 module WordN 
   ( WordN(..)
+  , NoMask(..)
   , maskWordN
   , wordNMask
+  , preserveBits
+  , preserveBitsR
   ) where
 
 import Data.Bits
@@ -64,14 +67,28 @@ instance (KnownNat n, n <= WordSize.MaxBits) => Enum (WordN n) where
     | x <= fromIntegral (maxBound :: Int) = I# (word2Int# (WordSize.unliftWord x'))
     | otherwise = fromEnumError "WordN" x
 
-  enumFrom          = integralEnumFrom
-  enumFromThen      = integralEnumFromThen
-  enumFromTo        = integralEnumFromTo
-  enumFromThenTo    = integralEnumFromThenTo
+  enumFrom       = integralEnumFrom
+  enumFromThen   = integralEnumFromThen
+  enumFromTo     = integralEnumFromTo
+  enumFromThenTo = integralEnumFromThenTo
 
 instance forall n. (KnownNat n, n <= WordSize.MaxBits) => Bounded (WordN n) where
   minBound = 0
   maxBound = wordNMask
+
+--------------------------------------------------------------------------
+
+newtype NoMask (n :: Nat) = NoMask { noMask :: WordSize.T }
+
+instance forall n. (KnownNat n , n <= WordSize.MaxBits) => Show (NoMask n) where
+  show (NoMask w) = show $ (maskWordN w :: WordN n)
+
+deriving instance (n <= WordSize.MaxBits) => Eq (NoMask n)
+deriving instance (n <= WordSize.MaxBits) => Ord (NoMask n)
+
+instance forall n. (KnownNat n, n <= WordSize.MaxBits) => Bounded (NoMask n) where
+  minBound = NoMask 0
+  maxBound = NoMask $ getWord (wordNMask :: WordN n)
 
 {-# INLINE wordNMask #-}
 -- | An (WordN n) with all the bits set, used for masking.
@@ -84,3 +101,16 @@ wordNMask = WordN . (flip (-) 1) . bit $ fromIntegral bits
 -- | Smart constructor for WrapWords which masks off the unused upper bits.
 maskWordN :: forall n. (KnownNat n) => WordSize.T -> WordN n
 maskWordN w = WordN $ w .&. getWord (wordNMask :: WordN n)
+
+{-# INLINE preserveBits #-}
+preserveBits :: forall n. KnownNat n => (WordSize.T -> WordSize.T) -> NoMask n -> NoMask n
+preserveBits f w = preserveBitsR (\_ w2 -> f w2) (NoMask 0) w
+
+{-# INLINE preserveBitsR #-}
+preserveBitsR :: forall n. KnownNat n => (WordSize.T -> WordSize.T -> WordSize.T) -> NoMask n -> NoMask n -> NoMask n
+preserveBitsR f (NoMask w1) (NoMask w2) = 
+  let w1' = getWord (maskWordN w1 :: WordN n)
+      w2' = getWord (maskWordN w2 :: WordN n)
+      mask = getWord (wordNMask :: WordN n)
+      upper = w2 .&. complement mask
+   in NoMask $ (f w1' w2') .|. upper
